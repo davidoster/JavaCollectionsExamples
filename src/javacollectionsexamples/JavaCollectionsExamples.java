4b32c5384516e1d991a7da40e8cc3a971c0a3ba3
David Oster <paspa@hotmail.com>
2020-07-24 22:22:17 +0300

    Set

--- src/javacollectionsexamples/JavaCollectionsExamples.java
+++ src/javacollectionsexamples/JavaCollectionsExamples.java
@@ -13,7 +13,8 @@ import java.util.TreeSet;
 import javacollectionsexamples.abstracts.Human;
 import models.Employee;
 import models.Student;
-import java.lang.Comparable;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -30,34 +31,63 @@ public class JavaCollectionsExamples {
         List<Human> emps2 = new ArrayList<>();
         Set<Human> setEmps = new HashSet<>();
         Set<Human> setTreeEmps = new TreeSet<>();
+        Map<Human,Student> xxx = new HashMap<>();
         
-        Employee emp = new Employee();
-        Human emp1 = new Employee(); // polymorphism
-        System.out.println(emp1);
-
-        emps.add(emp); // WRONG emps.add(emp1);
-        employees.add(emp1); // 
-        //CORRECT 
-        employees.add(emp); 
-        // WHY?????
-        emp1.talks("sdfsdf");
+        // Map<Student, Assignment> studentAssignments
+        // Map<CodingSchool, Trainer> trainers
+        // Map<Student, Course>
+        // Map<Course, List<Student>> courseStudents
         
-        setEmps.add(emp1);
-        setEmps.add(new Student());
-        System.out.println(setEmps.size());
+        Map<Object, List<Student>> xyz = new HashMap();
         
-        setTreeEmps.add(emp1);
-        setTreeEmps.add(emp);
-        setTreeEmps.add(new Student());
-        for (Human setTreeEmp : setTreeEmps) {
-            System.out.println(setTreeEmp);
+//        class Tmhma {
+//            List<Trainer>
+//            List<Student>
+//            Course
+//            List<Assignment>
+//        }
+//            
+//        class CodingSchool {
+//            List<Tmhma> tmhmata;
+//        }
+                    
+        
+        xxx.put(new Student(), new Student());
+        xxx.put(new Student(), new Student());
+        xxx.put(new Student(), new Student());
+        xxx.put(new Student(), new Student());
+        
+        for (Student s : xxx.values()) {
+            System.out.println(s);
         }
+   
+//        Employee emp = new Employee();
+//        Human emp1 = new Employee(); // polymorphism
+//        System.out.println(emp1);
+//
+//        emps.add(emp); // WRONG emps.add(emp1);
+//        employees.add(emp1); // 
+//        //CORRECT 
+//        employees.add(emp); 
+//        // WHY?????
+//        emp1.talks("sdfsdf");
+//        
+//        setEmps.add(emp1);
+//        setEmps.add(new Student());
+//        System.out.println(setEmps.size());
         
-        System.out.println(setEmps);
-        System.out.println(setEmps.hashCode());
-        for (Human h : setEmps) {
-            System.out.println(h.hashCode());
-        }
+//        setTreeEmps.add(emp1);
+//        setTreeEmps.add(emp);
+//        setTreeEmps.add(new Student());
+//        for (Human setTreeEmp : setTreeEmps) {
+//            System.out.println(setTreeEmp);
+//        }
+        
+//        System.out.println(setEmps);
+//        System.out.println(setEmps.hashCode());
+//        for (Human h : setEmps) {
+//            System.out.println(h.hashCode());
+//        }
 
         
     }
